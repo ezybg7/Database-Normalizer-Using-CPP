219c27cf9e8c4f8e54c82736f84bdf57415b3394
yfu03 <yannisfu@gmail.com>
2023-10-17 18:43:36 -0500

    updated convertTo1NF *known error*

--- nf1.h
+++ nf1.h
@@ -19,25 +19,7 @@ vector<string> splitString(const string& input, char delimiter) {
     return tokens;
 }
 
-vector<string> convertTo1NF(const vector<string>& dataset) {
-    unordered_map<string, vector<string>> resultMap;
-
-    //Iterate through the dataset and organize data based on keys
-    for (const auto& row : dataset) {
-        vector<string> values = splitString(row, ',');
-
-        //Use the first column as the key (superkey) for organizing data
-        string key = values[0];
-        if (resultMap.find(key) == resultMap.end()) {
-            resultMap[key] = values;
-        } else {
-            //Merge rows with the same key by concatenating values
-            for (size_t i = 1; i < values.size(); ++i) {
-                resultMap[key].push_back(values[i]);
-            }
-        }
-    }
-
+vector<string> convertTo1NF(const unordered_map<string, vector<string>> resultMap) {
     //Convert the map back to 1NF
     vector<string> result;
     for (const auto& pair : resultMap) {
