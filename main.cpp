7fc700b499b175909ec59c8aff9a8cb0a72747bd
yfu03 <yannisfu@gmail.com>
2023-10-24 22:17:21 -0500

    deleted old code

--- main.cpp
+++ main.cpp
@@ -7,22 +7,6 @@ int main(){
   const string file = "exampleInputTable.csv";
   const string fileFD = "functionalDependencies.txt";
 
-  //opening file
-  ifstream input;
-  string line;
-  input.open(fileFD);
-
-  //getting functional dependencies
-  //fixme : add functional dependences to the table after it has been made
-  vector<string> funcDeps;
-  if (input.is_open())
-  {
-    while( getline(input,line) )
-    {
-      funcDeps.push_back(line);
-    }
-    input.close();
-  }
 
   // vector<string> result = convertTo1NF(tupleMap);
 
