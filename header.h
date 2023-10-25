960d9f26d343127050a20bf29d1eead7b4c68d2d
Everett Yan <everettzyan@gmail.com>
2023-10-24 21:38:05 -0500

    possibly working?

--- header.h
+++ header.h
@@ -9,97 +9,63 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <numeric>
-#include <algorithm>
 
 using namespace std;
 //table struct so we can store each table
 struct Table {
     string name;
-    vector<string> attributes, keys, fundamentalDep;
+    vector<string> attributes;
+    vector<string> keys;
+    vector<string> fd;
     unordered_map<string, vector<string>> data;
 
     static int tableCount;
 
     //constructor that increments table
-    Table(const vector<string>& tableAttributes, const vector<string> fd, const vector<string>& tableKeys)
-        : name("Table" + to_string(++tableCount)), attributes(tableAttributes), fundamentalDep(fd), keys(tableKeys)  {}
+    Table(const vector<string>& tableAttributes, const vector<string>& tableKeys, const vector<string>& fucntionalDependencies)
+        : name("Table" + to_string(++tableCount)), attributes(tableAttributes), keys(tableKeys), fd(fucntionalDependencies) {}
 
     //parser
-    static Table parseCSV(const string& filename, const string& fileFD) {
-      
-      ifstream parseFD(fileFD);
-      string line2;
-      vector<string> FDinput;
-      if (parseFD.is_open())
       {
         while( getline(parseFD,line2) )
           FDinput.push_back(line2);
         parseFD.close();
       }
+    vector<string> attributes;
 
-      ifstream file(filename);
-      string line;
-      getline(file, line);
-
-      stringstream ss(line);
-      string attribute;
-      vector<string> attributes;
-
-      while (getline(ss, attribute, ',')) {
-          attributes.push_back(attribute);
-      }
-      
-
-      Table table(attributes, FDinput, {});
-
-      while (getline(file, line)) {
-
-        stringstream ss(line);
-        string value;
-        vector<string> dataRow;
-        int index = 0;
-
-        while (getline(ss, value, ',')) {
-            if (index >= attributes.size()) {
-                cerr << "Error: Number of columns in a row exceeds the number of attributes." << endl;
-                return table;
-            }
-
-            //parses for multivalued
-            if (value.front() == '"' && value.back() == '"')
-                value = value.substr(1, value.size() - 2);
-
-            stringstream valueStream(value);
-            string singleValue;
-
-            while (getline(valueStream, singleValue, '\"'))
-                table.data[attributes[index]].push_back(singleValue);
-            index++;
-        }
-        file.close();
-        
-      }
-      
-      return table;
+    while (getline(ss, attribute, ',')) {
+      attributes.push_back(attribute);
     }
+
+    Table table(attributes);
+
+    //reads rest of the data
+    while (getline(file, line)) {
+      stringstream ss(line);
+      string value;
+      vector<string> dataRow;
+
+      while (getline(ss, value, ',')) {
+        //detects multivalue
+        if (value.front() == '\"' && value.back() == '\"')
+          dataRow.push_back(value.substr(1, value.size() - 2));
+        else
+          dataRow.push_back(value);
+      }
+
+      if (dataRow.size() != attributes.size()) {
+        cerr << "Error: Number of columns in a row does not match the number of attributes." << endl;
+        return table;
+      }
+
+      //store data into table
+      for (size_t i = 0; i < attributes.size(); ++i) {
+        table.data[attributes[i]].push_back(dataRow[i]);
+      }
+    }
+
+    return table;
+  }
 };
-
-// //creates unordered map using attributes and tuples, essentially creating each collumn. 
-// unordered_map<string, vector<string>> mapReturn(vector<vector<string>> tuples, vector<string> attributes)
-// {
-//   unordered_map<string, vector<string>> resultMap;
-
-//   for(int i = 0; i < attributes.size(); i++)
-//   {
-//     string key = attributes[i];
-//     for(int j = 0; j < tuples.size(); j++)
-//     {
-//       resultMap[key].push_back(tuples[j][i]);
-//       //cout << resultMap[key].back() << endl;
-//     }
-//   }
-//   return resultMap;
-// }
 
 #endif
