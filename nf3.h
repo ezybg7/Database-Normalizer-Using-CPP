da0b574b3c822a892ac2df03ae0134ae590188fd
tfnkv <tonyf16665@gmail.com>
2023-11-01 15:57:47 -0500

    Ironed out 3NF
    Cleaned up some code

--- nf3.h
+++ nf3.h
@@ -3,15 +3,17 @@
 
 #include "header.h"
 #include "output.h"
+#include "nf1.h"
+#include "nf2.h"
 
 Table convertTo3NF(Table inputTable)
 {
-    if (!is1NF(inputTable))
+    for(const Table* i: input)
     {
-        return inputTable;
+        if(!is1NF(i))
+            i = convertTo1NF(i);
     }
 
-    //Convert to 2NF
     Table tableIn2NF = convertTo2NF(inputTable);
 
     // Extract primary key and non-key attributes
@@ -24,48 +26,10 @@ Table convertTo3NF(Table inputTable)
             nonKeyAttributes.push_back(attribute);
         }
     }
+    vector<Table> result;
 
-    unordered_map<string, Table> tables;
+    return result;
 
-    for (const auto &attribute : nonKeyAttributes)
-    {
-        //Create a new table with primary key and the current attribute
-        vector<string> tableAttributes = primaryKey;
-        tableAttributes.push_back(attribute);
-        Table newTable(tableAttributes, tableIn2NF.fundamentalDep, primaryKey, {});
-        tables[attribute] = newTable;
-    }
-
-    //Add data into 3NF tables
-    for (size_t i = 0; i < tableIn2NF.data[tableIn2NF.attributes[0]].size(); i++)
-    {
-        // Extract primary key values
-        vector<string> keyValues;
-        for (const auto &key : primaryKey)
-        {
-            keyValues.push_back(tableIn2NF.data[key][i]);
-        }
-
-        //Add non-key attribute values to respective 3NF tables
-        for (const auto &attribute : nonKeyAttributes)
-        {
-            string value = tableIn2NF.data[attribute][i];
-            vector<string> row = keyValues;
-            row.push_back(value);
-            tables[attribute].addRow(row);
-        }
-    }
-
-    //Create a new 3NF table
-    Table newTable(tableIn2NF.keys, tableIn2NF.fundamentalDep, tableIn2NF.keys, {});
-
-    //Merge 3NF tables into the new table
-    for (const auto &pair : tables)
-    {
-        newTable.mergeTable(pair.second);
-    }
-
-    return newTable;
 }
 
 #endif
